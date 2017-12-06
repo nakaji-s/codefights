e4fbbe4e05c24aec0e6803144006cc7dad3ae7ee
nakaji-s <nakajima.sota.sub@gmail.com>
2017-12-06 23:11:00 +0900

    Add 25.arrayReplace

---
+++ Intro/25.arrayReplace.go
@@ -0,0 +1,10 @@
+package main
+
+func arrayReplace(inputArray []int, elemToReplace int, substitutionElem int) []int {
+	for i, _ := range inputArray {
+		if inputArray[i] == elemToReplace {
+			inputArray[i] = substitutionElem
+		}
+	}
+	return inputArray
+}
