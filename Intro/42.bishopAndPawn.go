709eb3c74f8255ac658e0cfc8a2b5f8bf8c6dd67
nakaji-s <nakajima.sota.sub@gmail.com>
2017-12-14 19:05:14 +0900

    Add 42.bishopAndPawn

---
+++ Intro/42.bishopAndPawn.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"math"
+)
+
+func bishopAndPawn(bishop string, pawn string) bool {
+	x := int(pawn[0]) - int(bishop[0])
+	y := int(pawn[1]) - int(bishop[1])
+	return math.Abs(float64(x)) == math.Abs(float64(y))
+}
